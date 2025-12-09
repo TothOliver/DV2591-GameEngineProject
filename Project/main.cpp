9f981018deb124f8ca9d48e84d180a55f6e41319
Ponlin02 <ponlin.02@gmail.com>
2025-12-09 14:46:20 +0100

    Moved assets and main fixes

--- Project/main.cpp
+++ Project/main.cpp
@@ -7,6 +7,7 @@
 #include "raylib.h"
 
 #include <iostream>
+#include <string>
 #include <memory>
 
 void SetTexture(Model& model, std::shared_ptr<IResource>& baseRes)
@@ -40,6 +41,13 @@ void SetTexture(Model& model, std::shared_ptr<IResource>& baseRes)
                 texture = LoadTextureFromImage(img);
 
                 std::cout << "Async texture ready: " << width << "x" << height << "\n";
+
+                Texture2D oldTexture = model.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture;
+                if (oldTexture.id != 1) 
+                {
+                    UnloadTexture(oldTexture);
+                }
+
                 model.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = texture;
             }
         }
@@ -68,6 +76,13 @@ void SetTexture(Model& model, std::shared_ptr<IResource>& baseRes)
                 texture = LoadTextureFromImage(img);
 
                 std::cout << "Async texture ready: " << width << "x" << height << "\n";
+
+                Texture2D oldTexture = model.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture;
+                if (oldTexture.id != 1)
+                {
+                    UnloadTexture(oldTexture);
+                }
+
                 model.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = texture;
             }
         }
@@ -81,8 +96,9 @@ int main()
     packagingTool.buildPackage("AssetsTextList.txt", "Assets.bundle");
 
     //Asset manager
-    AssetManager am(64 * 1024 * 1024, "Assets.bundle");
+    AssetManager am(10 * 1024 * 1024, "Assets.bundle");
 
+    //Start loading some stuff
     am.LoadAsync("001");
     am.LoadAsync("002");
     am.LoadAsync("003");
@@ -112,6 +128,11 @@ int main()
     auto sphereMesh = std::dynamic_pointer_cast<MeshObj>(roundObj);
     Model sphere = ConvertAttribToModel(sphereMesh->GetAttrib(), sphereMesh->GetShapes());
 
+    //progressive stuff
+
+    std::string LODName;
+    bool higherLODRequested = false;
+
     //Camera creation (obviously)
     Camera camera = { 0 };
     camera.position = { 0.0f, 0.0f, 10.0f };
@@ -120,8 +141,8 @@ int main()
     camera.fovy = 45.0f;
     camera.projection = CAMERA_PERSPECTIVE;
 
-    Texture2D rayTex{};
     bool rayTextureReady = false;
+    float LODTimer = 0.0;
 
     while (!WindowShouldClose())
     {
@@ -163,10 +184,77 @@ int main()
 
         if (IsKeyPressed(KEY_FOUR))
         {
-            //am.LoadAsync("004_lod0");
+            am.LoadAsync("004_lod0");
             std::shared_ptr<IResource> myResource = am.TryGet("004_lod0");
-            SetTexture(bigBox, myResource);
+
+            if (myResource) {
+                SetTexture(bigBox, myResource);
+
+                auto progRes = std::dynamic_pointer_cast<ProgressiveTexturePng>(myResource);
+                if (progRes) {
+                    progRes->SetLODInfo(2);
+                    LODName = progRes->GetNextLODGuid();
+                    am.LoadAsync(LODName);
+                    higherLODRequested = true;
+                    LODTimer = 0.0f;
+                }
+            }
         }
+
+        if (IsKeyPressed(KEY_FIVE))
+        {
+            for (int i = 200; i < 220; i++)
+            {
+                am.LoadAsync(std::to_string(i));
+            }
+        }
+
+        if (higherLODRequested)
+        {
+            LODTimer += GetFrameTime();
+
+            if (LODTimer >= 2.0f)
+            {
+                auto higherLODRes = am.TryGet(LODName);
+
+                if (higherLODRes) {
+                    auto currentRes = am.TryGet("004_lod0");
+                    auto currentTex = std::dynamic_pointer_cast<ProgressiveTexturePng>(currentRes);
+                    auto higherTex = std::dynamic_pointer_cast<ProgressiveTexturePng>(higherLODRes);
+
+                    if (currentTex && higherTex)
+                    {
+                        bool loaded = currentTex->LoadHigherLOD(
+                            std::vector<uint8_t>(higherTex->GetImageData(),
+                                higherTex->GetImageData() + (higherTex->GetWidth() * higherTex->GetHeight() * 4)),
+                            higherTex->GetWidth(),
+                            higherTex->GetHeight()
+                        );
+
+                        if (loaded)
+                        {
+                            currentTex->TryUpgrade();
+                            SetTexture(bigBox, currentRes);
+                            
+                            am.Unload(LODName);
+
+                            if (currentTex->HasHigherLOD())
+                            {
+                                LODName = currentTex->GetNextLODGuid();
+                                am.LoadAsync(LODName);
+                                LODTimer = 0.0f;
+                            }
+                            else
+                            {
+                                higherLODRequested = false;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+
 
         if (IsKeyPressed(KEY_X))
         {
@@ -181,6 +269,9 @@ int main()
             am.Unload("001");
             am.Unload("002");
             am.Unload("003");
+            am.Unload("004_lod0");
+
+            UnloadTexture(empty);
         }
 
         //Background
@@ -208,14 +299,27 @@ int main()
         EndMode3D();
 
         //2D
-        DrawRectangle(20, 20, 330, 100, Fade(SKYBLUE, 0.5f));
-        DrawRectangleLines(20, 20, 330, 100, BLUE);
+        DrawRectangle(20, 20, 330, 170, Fade(SKYBLUE, 0.5f));
+        DrawRectangleLines(20, 20, 330, 170, BLUE);
 
         DrawText("Controls:", 30, 30, 30, BLACK);
         DrawText("1-3 -> Load textures", 30, 60, 30, BLACK);
-        DrawText("X -> Clean all", 30, 90, 30, BLACK);
+        DrawText("4 -> Multiresolution", 30, 90, 30, BLACK);
+        DrawText("5 -> Load much", 30, 120, 30, BLACK);
+        DrawText("x -> Clean all", 30, 150, 30, BLACK);
 
         EndDrawing();
+    }
+
+    std::vector<Model> models = { dynamicModel, background, box1, box2, box3, box4, box5, bigBox, sphere };
+    for (int i = 0; i < models.size(); i++)
+    {
+        Texture2D oldTexture = models[i].materials[0].maps[MATERIAL_MAP_DIFFUSE].texture;
+        if (oldTexture.id != 1)
+        {
+            UnloadTexture(oldTexture);
+        }
+        UnloadModel(models[i]);
     }
 
     CloseWindow();
